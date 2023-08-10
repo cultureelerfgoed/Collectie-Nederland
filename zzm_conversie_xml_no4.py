de252bb351de75687e6aeaf650093da912866e78
Joop Vanderheiden <jolietjakeblues64@gmail.com>
2023-08-10 15:47:29 +0200

    aanpassinen

--- zzm_conversie_xml_no4.py
+++ zzm_conversie_xml_no4.py
@@ -1,14 +1,19 @@
-
-# Vierde conversie verwijderen tags <sys:Object> en </sys:Object>
-
 import re
 
+# Vraag de gebruiker om de naam van het inputbestand
+input_filename = input("Voer de naam van het inputbestand in (bijv. zzm_aangepast_stap3_xml.rdf): ")
+
+# Vraag de gebruiker om de naam van het outputbestand
+output_filename = input("Voer de naam van het outputbestand in (bijv. zzm_aangepast_stap4_xml.rdf): ")
+
 # Lees het oorspronkelijke XML-bestand
-with open('/Users/patrickmout/Downloads/zzm/zzm_aangepast_stap3_xml.rdf', 'r') as file:
+with open(input_filename, 'r', encoding='utf-8') as file:
     rdf_content = file.read()
 
 # Definieer het patroon voor <sys:Object> tags
 sys_object_pattern = r'<sys:Object>|</sys:Object>'
+
+print("Verwijderen van <sys:Object> en </sys:Object> tags...")
 
 # Verwijder de <sys:Object> en </sys:Object> tags
 modified_content = re.sub(sys_object_pattern, '', rdf_content)
@@ -16,7 +21,10 @@ modified_content = re.sub(sys_object_pattern, '', rdf_content)
 # Verwijder overgebleven lege regels
 modified_content = re.sub(r'\n\s*\n', '\n', modified_content)
 
+print("Schrijven naar outputbestand...")
 
 # Schrijf het aangepaste XML-bestand
-with open('/Users/patrickmout/Downloads/zzm/zzm_aangepast_stap4_xml.rdf', 'w') as file:
+with open(output_filename, 'w', encoding='utf-8') as file:
     file.write(modified_content)
+
+print("Conversie voltooid!")
