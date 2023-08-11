e4b15aa3630e3a2be161abf31a0ab609d7e6e773
Joop Vanderheiden <jolietjakeblues64@gmail.com>
2023-08-11 15:30:46 +0200

    aanpassingen 11-8

--- hvh_conversie_xml_no3.py
+++ hvh_conversie_xml_no3.py
@@ -1,4 +1,5 @@
 import re
+from tqdm import tqdm
 
 # Vraag de gebruiker om de naam van het inputbestand
 input_file_name = input("Voer de naam van het inputbestand in: ")
@@ -16,20 +17,17 @@ object_blocks = re.findall(r'<sys:Object>.*?</sys:Object>', rdf_content, re.DOTA
 total_blocks = len(object_blocks)
 print(f"Totaal aantal <sys:Object> blokken gevonden: {total_blocks}")
 
-# Lus door de gevonden blokken
-for index, object_block in enumerate(object_blocks, 1):
+# Lus door de gevonden blokken met tqdm voor voortgangsindicator
+for object_block in tqdm(object_blocks, desc="Verwerken", unit="blok"):
     # Zoek de laatste </skos:Concept> tag binnen elk object_block
-    last_skos_concept_index = object_block.rfind('</skos:Concept>')
+    last_skos_concept_index = object_block.rfind('</edm:Place>')
 
     if last_skos_concept_index != -1:
         # Vervang de laatste </skos:Concept> tag door </skos:Concept></dcterms:subject>
-        modified_block = object_block[:last_skos_concept_index] + '</skos:Concept>' + '\n' + '</edm:ProvidedCHO>' + object_block[last_skos_concept_index+len('</skos:Concept>'):]
+        modified_block = object_block[:last_skos_concept_index] + '</edm:Place>' + '\n' + '</edm:ProvidedCHO>' + object_block[last_skos_concept_index+len('</skos:Concept>'):]
 
         # Vervang het oorspronkelijke object_block door het gewijzigde object_block in rdf_content
         rdf_content = rdf_content.replace(object_block, modified_block)
-
-    # Print voortgangsindicator
-    print(f"Verwerkt: {index}/{total_blocks} blokken", end='\r')
 
 print("\nConversie voltooid!")
 
